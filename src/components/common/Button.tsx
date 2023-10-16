03cf629e17ead154787150562f367177d37c5780
Miandry05 <86937367+MiandryRakoto@users.noreply.github.com>
2023-10-16 19:02:24 +0300

    Component created

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,17 @@
+const Button = (props: any) => {
+  return (
+    <button
+      type="submit"
+      className={
+        props.width +
+        " " +
+        props.height +
+        " inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm white:focus:ring-offset-gray-800"
+      }
+    >
+      {props.name}
+    </button>
+  );
+};
+
+export default Button;
--- src/views/Authentication/RegisterAuth.tsx
+++ src/views/Authentication/RegisterAuth.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import teratanyLogo from "../../assets/Teratany_ico/apple-touch-icon-180x180.png";
+import FormField from "../../components/common/FormField";
+import Button from "../../components/common/Button";
 
 const RegisterAuth: React.FC = () => {
   return (
@@ -13,7 +15,7 @@ const RegisterAuth: React.FC = () => {
           <div className="p-4 sm:p-7">
             <div className="text-center">
               <h1 className="block text-2xl font-bold text-gray-800 white:text-white">
-                Sign up
+                Ready to get started? Sign up here!
               </h1>
               <p className="mt-2 text-sm text-gray-600 white:text-gray-400">
                 Already have an account ?
@@ -28,116 +30,31 @@ const RegisterAuth: React.FC = () => {
 
             <div className="mt-5">
               <form>
-                <div className="grid text-start gap-y-4">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm mb-2 white:text-white"
-                    >
-                      Email address
-                    </label>
-                    <div className="relative">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="custom-border py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400"
-                        required
-                        aria-describedby="email-error"
-                      />
-                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                        <svg
-                          className="h-5 w-5 text-red-500"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          viewBox="0 0 16 16"
-                          aria-hidden="true"
-                        >
-                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                        </svg>
-                      </div>
-                    </div>
-                    <p
-                      className="hidden text-xs text-red-600 mt-2"
-                      id="email-error"
-                    >
-                      Please include a valid email address so we can get back to
-                      you
-                    </p>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block text-sm mb-2 white:text-white"
-                    >
-                      Password
-                    </label>
-                    <div className="relative">
-                      <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        className="custom-border py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400"
-                        required
-                        aria-describedby="password-error"
-                      />
-                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                        <svg
-                          className="h-5 w-5 text-red-500"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          viewBox="0 0 16 16"
-                          aria-hidden="true"
-                        >
-                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                        </svg>
-                      </div>
-                    </div>
-                    <p
-                      className="hidden text-xs text-red-600 mt-2"
-                      id="password-error"
-                    >
-                      8+ characters required
-                    </p>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="confirm-password"
-                      className="block text-sm mb-2 white:text-white"
-                    >
-                      Confirm Password
-                    </label>
-                    <div className="relative">
-                      <input
-                        type="password"
-                        id="confirm-password"
-                        name="confirm-password"
-                        className="py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400"
-                        required
-                        aria-describedby="confirm-password-error"
-                      />
-                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                        <svg
-                          className="h-5 w-5 text-red-500"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          viewBox="0 0 16 16"
-                          aria-hidden="true"
-                        >
-                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                        </svg>
-                      </div>
-                    </div>
-                    <p
-                      className="hidden text-xs text-red-600 mt-2"
-                      id="confirm-password-error"
-                    >
-                      Password does not match the password
-                    </p>
-                  </div>
+                <div className="grid text-start gap-y-2">
+                  <FormField
+                    label="Email Address"
+                    type="email"
+                    mark="email"
+                    height="py-3"
+                    width="px-4"
+                    extra={false}
+                  />
+                  <FormField
+                    label="Name"
+                    type="texte"
+                    mark="name"
+                    height="py-3"
+                    width="px-4"
+                    extra={false}
+                  />
+                  <FormField
+                    label="Password"
+                    type="password"
+                    mark="psswrd"
+                    height="py-3"
+                    width="px-4"
+                    extra={false}
+                  />
                   <div className="flex items-center">
                     <div className="flex">
                       <input
@@ -156,13 +73,7 @@ const RegisterAuth: React.FC = () => {
                       </label>
                     </div>
                   </div>
-
-                  <button
-                    type="submit"
-                    className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm white:focus:ring-offset-gray-800"
-                  >
-                    Sign up
-                  </button>
+                  <Button width="px-4" height="py-3" name="Sign up" />
                 </div>
               </form>
             </div>
