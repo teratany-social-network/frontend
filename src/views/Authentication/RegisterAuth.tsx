a8c7a830ee835f2e21d41bf397842f556afad7e0
Miandry05 <86937367+MiandryRakoto@users.noreply.github.com>
2023-10-16 11:11:56 +0300

    Authentication integration

--- src/views/Authentication/RegisterAuth.tsx
+++ src/views/Authentication/RegisterAuth.tsx
@@ -1,9 +1,174 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import teratanyLogo from "../../assets/Teratany_ico/apple-touch-icon-180x180.png";
 
 const RegisterAuth: React.FC = () => {
   return (
-    <div>
-      <h2>Register Auth</h2>
+    <div className="h-screen w-full flex items-center justify-center white:bg-slate-900 bg-gray-100 py-16">
+      <main className="w-full max-w-md mx-auto p-6">
+        <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm white:bg-gray-800 white:border-gray-700">
+          <div className="flex justify-center">
+            <img src={teratanyLogo} alt="" className=" w-20 h-20" />
+          </div>
+          <div className="p-4 sm:p-7">
+            <div className="text-center">
+              <h1 className="block text-2xl font-bold text-gray-800 white:text-white">
+                Sign up
+              </h1>
+              <p className="mt-2 text-sm text-gray-600 white:text-gray-400">
+                Already have an account ?
+                <Link
+                  className="text-blue-600 decoration-2 hover:underline font-medium ml-1"
+                  to="/signin"
+                >
+                  Sign in here
+                </Link>
+              </p>
+            </div>
+
+            <div className="mt-5">
+              <form>
+                <div className="grid text-start gap-y-4">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm mb-2 white:text-white"
+                    >
+                      Email address
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="custom-border py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400"
+                        required
+                        aria-describedby="email-error"
+                      />
+                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                        <svg
+                          className="h-5 w-5 text-red-500"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          aria-hidden="true"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p
+                      className="hidden text-xs text-red-600 mt-2"
+                      id="email-error"
+                    >
+                      Please include a valid email address so we can get back to
+                      you
+                    </p>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm mb-2 white:text-white"
+                    >
+                      Password
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="custom-border py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400"
+                        required
+                        aria-describedby="password-error"
+                      />
+                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                        <svg
+                          className="h-5 w-5 text-red-500"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          aria-hidden="true"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p
+                      className="hidden text-xs text-red-600 mt-2"
+                      id="password-error"
+                    >
+                      8+ characters required
+                    </p>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="confirm-password"
+                      className="block text-sm mb-2 white:text-white"
+                    >
+                      Confirm Password
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="password"
+                        id="confirm-password"
+                        name="confirm-password"
+                        className="py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400"
+                        required
+                        aria-describedby="confirm-password-error"
+                      />
+                      <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                        <svg
+                          className="h-5 w-5 text-red-500"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          aria-hidden="true"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        </svg>
+                      </div>
+                    </div>
+                    <p
+                      className="hidden text-xs text-red-600 mt-2"
+                      id="confirm-password-error"
+                    >
+                      Password does not match the password
+                    </p>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="flex">
+                      <input
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                        className="shrink-0 mt-0.5 border border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:checked:bg-blue-500 white:checked:border-blue-500 white:focus:ring-offset-gray-800"
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <label
+                        htmlFor="remember-me"
+                        className="text-sm white:text-white"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm white:focus:ring-offset-gray-800"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
