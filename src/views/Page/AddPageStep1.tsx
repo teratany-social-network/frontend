e331c4e8fc122a3e36642be772e50f28f2368db9
andritianaa <andritianasteve@gmail.com>
2023-10-29 00:39:55 +0300

    integration add page

--- src/views/Page/AddPageStep1.tsx
+++ src/views/Page/AddPageStep1.tsx
@@ -5,6 +5,7 @@ import Button from "../../components/common/Button";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { InfoModal } from "../../components/InfoModal";
 
 interface addNewPageField {
   name: string;
@@ -27,6 +28,8 @@ const AddPageStep1: React.FC = () => {
   return (
     <>
       <TopBar text="Add new page" />
+      <InfoModal title="Register your organization" text="Welcome to the page where you can register your association or business for CSR. You are invited to fill in the required information, except for those marked as optional. This information will only be made available to users to help them recognize you quickly and will not be used for advertising purposes under any circumstances." />
+
       <div className="mt-16 overflow-y-scroll flex flex-col items-center mx-4">
         <Formik
           initialValues={initialValues}
@@ -44,6 +47,13 @@ const AddPageStep1: React.FC = () => {
           }}
         >
           <Form className="w-3/4">
+            <label htmlFor="pageType" className="text-left block text-sm white:text-white my-2">
+              Your organization type
+            </label>
+            <select name="pageType" className="py-2 custom-border px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400" id="pageType">
+              <option value="association">Association</option>
+              <option value="entreprise">Entreprise</option>
+            </select>
             <FormField
               label="Name"
               type="name"
@@ -71,8 +81,9 @@ const AddPageStep1: React.FC = () => {
               extra={false}
               extraDesc="Phone number"
             />
+
             <FormField
-              label="website"
+              label="website (optional)"
               type="url"
               mark="text"
               height="py-2"
@@ -80,10 +91,19 @@ const AddPageStep1: React.FC = () => {
               extra={false}
               extraDesc="Website"
             />
+
+            <label htmlFor="description" className="text-left block text-sm white:text-white my-2">
+              Description
+            </label>
+            <textarea name="description" rows={4} className="py-2 custom-border px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 white:bg-gray-800 white:border-gray-700 white:text-gray-400 h-24 max-h-24" id="description">
+            </textarea>
           </Form>
         </Formik>
-        <div className="my-10">
-          <Button width="px-4" height="py-3" name="Next step" onClick={() => navigate('/add-page/step-2')} />
+        <div className="my-6 w-3/4">
+          <Button width="w-full" height="py-3" name="Next step" onClick={() => navigate('/add-page/step-2')} />
+          <button className="w-full text-gray-500 bg-white  rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 mt-4" onClick={() => navigate('/user')}>
+            Cancel
+          </button>
         </div>
       </div>
     </>
--- src/views/Page/AddPageStep2.tsx
+++ src/views/Page/AddPageStep2.tsx
@@ -1,16 +1,20 @@
 import Button from "../../components/common/Button";
 import MapContainerForm from "../../components/MapContainer";
+import { InfoModal } from "../../components/InfoModal";
+import { useNavigate } from "react-router-dom";
 
 const AddPageStep2: React.FC = () => {
+    const navigate = useNavigate()
     return (
         <>
             <MapContainerForm className="w-full h-screen">
             </MapContainerForm>
             <div className="fixed bottom-0 z-1000 flex items-center p-4 w-full">
-                <button className="bg-black text-white py-3 rounded-md w-full">
+                <button className="bg-black text-white py-3 rounded-md w-full" onClick={() => navigate('/add-page/step-3')}>
                     Save my position
                 </button>
             </div>
+            <InfoModal title="Select you position" text=" Enter the location of your organization by selecting it on the following interactive map. Organizations registering on our platform are required to be transparent. Your data will be used solely to inform users interested in your organization about your location and will not be used for advertising purposes." />
         </>
     )
 }
