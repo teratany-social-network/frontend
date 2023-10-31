48075dcc8f97058452d481cf504752b0e7326a54
nyhasina <nyhasina.finaritra@gmail.com>
2023-10-31 20:02:51 +0100

    add page done

---
+++ src/constants/PageCategory.ts
@@ -0,0 +1,128 @@
+const CategorieList = [
+    [
+        {
+            value: "b",
+            text: "No poverty",
+        },
+        {
+            value: "c",
+            text: "Zero hunger",
+        },
+        {
+            value: "d",
+            text: "Good health and well-being",
+        },
+        {
+            value: "e",
+            text: "Quality education",
+        },
+        {
+            value: "f",
+            text: "Gender equality",
+        },
+        {
+            value: "g",
+            text: "Clear water and sanitation",
+        },
+        {
+            value: "h",
+            text: "Affordable and clean energy",
+        },
+        {
+            value: "i",
+            text: "Decent work and growth",
+        },
+        {
+            value: "j",
+            text: "Industry, innovation and infrastructure",
+        },
+        {
+            value: "k",
+            text: "Reduce inequalities",
+        },
+        {
+            value: "l",
+            text: "Sustainable cities and communities",
+        },
+        {
+            value: "m",
+            text: "Responsable consumption and production",
+        },
+        {
+            value: "n",
+            text: "Climate action",
+        },
+        {
+            value: "o",
+            text: "Life below water",
+        },
+        {
+            value: "p",
+            text: "Life on land",
+        },
+        {
+            value: "q",
+            text: "Peace, justice",
+        },
+        {
+            value: "a",
+            text: "Partenerships for other goals",
+        },
+    ],
+    [
+        {
+            value: "a",
+            text: "Innovative",
+        },
+        {
+            value: "b",
+            text: "Attractive",
+        },
+        {
+            value: "c",
+            text: "Develops short circuits and responsible purchasing",
+        },
+        {
+            value: "d",
+            text: "Promotes employment in the region",
+        },
+        {
+            value: "e",
+            text: "Develop its competitiveness",
+        },
+        {
+            value: "f",
+            text: "Respects the environment and biodiversity",
+        },
+        {
+            value: "g",
+            text: "Promotes equality and diversity",
+        },
+        {
+            value: "h",
+            text: "Promotes safety and well-being at work",
+        },
+        {
+            value: "i",
+            text: "Contributes to the energy transition",
+        },
+        {
+            value: "j",
+            text: "Promotes the development of the circular economy",
+        },
+        {
+            value: "k",
+            text: "Fight against corruption",
+        },
+        {
+            value: "l",
+            text: "Open to dialogue and collaborative",
+        },
+        {
+            value: "m",
+            text: "Develop culture and risk management",
+        },
+    ],
+];
+
+export { CategorieList }
