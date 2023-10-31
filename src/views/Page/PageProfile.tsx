b76300a8867c079b88a55daaeb5ef25eb420d419
Miandry05 <86937367+MiandryRakoto@users.noreply.github.com>
2023-10-31 22:31:28 +0300

    DETAILS DONE

--- src/views/Page/PageProfile.tsx
+++ src/views/Page/PageProfile.tsx
@@ -3,19 +3,31 @@ import TopBar from "../../components/common/TopBar";
 import Button from "../../components/common/Button";
 import Publication from "../../components/Publication/Publication";
 import { BottomDrawer } from "../../components/common/BottomDrawer";
+import { BsInfoCircle } from "@react-icons/all-files/bs/BsInfoCircle";
+import { IoMailUnreadOutline } from "@react-icons/all-files/io5/IoMailUnreadOutline";
+import { BsPhone } from "@react-icons/all-files/bs/BsPhone";
+import { MdLocationCity } from "@react-icons/all-files/md/MdLocationCity";
+import { BsWallet } from "@react-icons/all-files/bs/BsWallet";
+import { GiWorld } from "@react-icons/all-files/gi/GiWorld";
+import { BiTargetLock } from "@react-icons/all-files/bi/BiTargetLock";
+import EditType from "../../components/EditType";
 
 const PageProfile: React.FC = () => {
-  const [drawerOpen, setDrawerOpen] = useState<boolean>(false)
-  const closeDrawer = () => setDrawerOpen(false)
-  const changeDrawerStatus = () => setDrawerOpen(true)
-  const DetailsComponent = <Details />
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+  const closeDrawer = () => setDrawerOpen(false);
+  const changeDrawerStatus = () => setDrawerOpen(true);
+  const DetailsComponent = <Details />;
   return (
     <>
       <TopBar text="Symbiozis" />
       <div className="mt-16 pb-6 border-b border-gray-200">
         <div className="flex items-start justify-evenly">
           <div className="flex flex-col">
-            <img className="w-20 h-20 object-cover rounded-full  border-2 border-pink-600 p-1" src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" alt="profile" />
+            <img
+              className="w-20 h-20 object-cover rounded-full  border-2 border-pink-600 p-1"
+              src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+              alt="profile"
+            />
             <p className="text-lg mb-3">Symbiozis</p>
           </div>
           <div className="flex flex-col">
@@ -60,7 +72,12 @@ const PageProfile: React.FC = () => {
         <div className="flex items-center mx-2">
           <Button width="w-1/2" height="h-7" name="Unfollow" />
           <Button width="w-1/2" height="h-7" name="message" />
-          <Button width="" height="h-7" name="Details" onClick={changeDrawerStatus} />
+          <Button
+            width=""
+            height="h-7"
+            name="Details"
+            onClick={changeDrawerStatus}
+          />
         </div>
       </div>
       <Publication />
@@ -69,17 +86,20 @@ const PageProfile: React.FC = () => {
       <Publication />
       <Publication />
 
-      <BottomDrawer isOpen={drawerOpen} onClose={closeDrawer} content={DetailsComponent} title="Details" />
+      <BottomDrawer
+        isOpen={drawerOpen}
+        onClose={closeDrawer}
+        content={DetailsComponent}
+        title="Details"
+      />
     </>
   );
 };
 
 export default PageProfile;
 
-
-
 interface IODD {
-  text: string
+  text: string;
 }
 export const ODD: React.FC<IODD> = ({ text }) => {
   return (
@@ -92,19 +112,72 @@ export const ODD: React.FC<IODD> = ({ text }) => {
 };
 
 const Details: React.FC = () => {
+  const pageType = <BsInfoCircle size={23} />;
+  const phone = <BsPhone size={23} />;
+  const email = <IoMailUnreadOutline size={23} />;
+  const location = <BiTargetLock size={23} />;
+  const wallet = <BsWallet size={23} />;
+  const address = <MdLocationCity size={23} />;
+  const website = <GiWorld size={23} />;
   return (
     <>
-      <div className=" flex flex-col items-center justify-center pb-16">
-        <div className="p-4 space-y-2">
-          <ODD text="No poverty" />
-          <ODD text="Zero hunger" />
-          <ODD text="Good health and well-being" />
-          <ODD text="Quality education" />
-          <ODD text="Gender equality" />
-          <ODD text="Clear water and sanitation" />
-          <ODD text="Affordable and clean energy" />
-          <ODD text="Decent work and growth" />
+      <div className="mt-2">
+        <EditType
+          name="Entreprise"
+          type="page"
+          path="/user/edit/general"
+          icon={pageType}
+        />
+        <div className=" rounded-md p-3 border my-2 ">
+          <div className="flex gap-3 items-center">
+            <h3 className="font-bold">Description</h3>
+          </div>
+
+          <p className="flex text-left text-gray-600 mt-2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ducimus
+            deserunt assumenda animi ullam nesciunt, nihil natus maxime, magni
+            qui repellat? Esse, odit animi! Accusantium voluptate consequatur
+            omnis perferendis perspiciatis optio dicta sapiente atque, tempora
+            vitae doloremque fugiat tenetur sunt.
+          </p>
         </div>
-      </div></>
-  )
-}
+        <EditType
+          name="Symbiozis@gmail.com"
+          type="page"
+          path="/user/edit/location"
+          icon={email}
+        />
+        <EditType
+          name="+261 34 24 734 56"
+          type="page"
+          path="/user/edit/picture"
+          icon={phone}
+        />
+        <EditType
+          name="Madagascar"
+          type="page"
+          path="/user/edit/picture"
+          icon={location}
+        />
+        <EditType
+          name="Lot II C 89 Z Ivandry"
+          type="page"
+          path="/user/edit/picture"
+          icon={address}
+        />
+        <EditType
+          name="https//Symbiozis.ca"
+          type="page"
+          path="/user/edit/picture"
+          icon={website}
+        />
+        <EditType
+          name="Wallet ID"
+          type="page"
+          path="/user/edit/picture"
+          icon={wallet}
+        />
+      </div>
+    </>
+  );
+};
