0abcd8aa29962602b7b5c3929bb7de0a5b13b1fa
andritianaa <andritianasteve@gmail.com>
2023-11-28 20:44:25 +0300

    BUG-2

--- src/api/FileApi.ts
+++ src/api/FileApi.ts
@@ -1,8 +1,7 @@
 import api from "api/api";
-const URLS = {
-    uploadFile: "/upload"
-}
+const URLS = { uploadFile: "/upload" }
 export const uploadFile = (images: any) => {
     return api.post(URLS.uploadFile, images)
 }
+
 export const FileServerURL = "https://file.teratany.org/public/"
