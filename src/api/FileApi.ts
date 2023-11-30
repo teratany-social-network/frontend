c7978e521fdec25378c50a16bd7f2083e8379284
andritianaa <andritianasteve@gmail.com>
2023-11-30 17:13:00 +0300

    tertany

--- src/api/FileApi.ts
+++ src/api/FileApi.ts
@@ -4,4 +4,4 @@ export const uploadFile = (images: any) => {
     return api.post(URLS.uploadFile, images)
 }
 
-export const FileServerURL = "https://file.teratany.org/public/"
+export const FileServerURL = "https://backend.teratany.org/public/"
