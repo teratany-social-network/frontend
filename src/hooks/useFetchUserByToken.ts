2f93272995b7140d7c0afe6b0552cb8702334964
nyhasina <nyhasina.finaritra@gmail.com>
2023-12-14 07:42:44 +0100

    bugf-8-erreur-en-html-lorsquon-ouvre-lapplication

--- src/hooks/useFetchUserByToken.ts
+++ src/hooks/useFetchUserByToken.ts
@@ -18,16 +18,17 @@ const useFetchUserByToken = () => {
 
 
         async function fetchUser() {
-            const { error, response } = await withAsync(() => getUserByToken(token));
-            if (error instanceof AxiosError) {
-                console.log(error)
-                const error_message: string =
-                    error?.response?.data?.error?.description || error?.response?.data || error.message;
-                toast.error(error_message);
-            } else {
-                setUser(response?.data as IProfile);
+            if (token) {
+                const { error, response } = await withAsync(() => getUserByToken(token));
+                if (error instanceof AxiosError) {
+                    console.log(error)
+                    const error_message: string =
+                        error?.response?.data?.error?.description || error?.response?.data || error.message;
+                    toast.error(error_message);
+                } else {
+                    setUser(response?.data as IProfile);
+                }
             }
-
         }
         fetchUser()
 
