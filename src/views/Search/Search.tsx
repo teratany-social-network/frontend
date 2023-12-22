1f4824c55ee3873f0035a8082b5c531c361f67ca
Ny Hasina Finaritra <84773267+Nyhasina23@users.noreply.github.com>
2023-12-22 22:22:08 +0300

    Front 6 logique historique recherche (#18)

--- src/views/Search/Search.tsx
+++ src/views/Search/Search.tsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../../components/SearchBar";
 import RecentCard from "../../components/RecentCard";
+import { withAsync } from "../../helpers/withAsync";
+import { getSearchHistory } from "../../api/SearchApi";
+import useToken from "../../hooks/useToken";
+import useFetchProfile from "../../hooks/useFetchProfile";
+import { ErrorData, ThrowErrorHandler } from "../../helpers/HandleError";
+import { IHistory } from "../../types/historique.type";
 
 const Search: React.FC = () => {
+  const token = useToken();
+  const [searchHistory, setSearchHistory] = useState<IHistory[]>();
+  const profileConnected = useFetchProfile();
+
+  const fetchUserSearchHistory = async () => {
+    if (profileConnected) {
+      const { error, response } = await withAsync(() =>
+        getSearchHistory(token, profileConnected?._id!)
+      );
+      if (error) {
+        ThrowErrorHandler(error as ErrorData);
+      } else {
+        setSearchHistory(response?.data as IHistory[]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserSearchHistory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [profileConnected?._id]);
+
   return (
     <>
       <div className="mx-3 mt-4 flex flex-col items-start">
         <SearchBar />
         <p className="pt-1 pb-3 text-ms font-medium">Recent</p>
       </div>
-      <RecentCard searchValue="Symbiozis" />
-      <RecentCard searchValue="Teratany" />
-      <RecentCard searchValue="Rakotoarivelo" />
-      <RecentCard searchValue="Africa" />
+
+      <RecentCard
+        historique={searchHistory!}
+        onClick={fetchUserSearchHistory}
+      />
     </>
   );
 };
